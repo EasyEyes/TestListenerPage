347e3d89f9abbbb11ed89843cc512d44f6c8e7b3
Nati Tsegaye <nht249@nyu.edu>
2025-01-07 20:12:00 +0400

    serve static files

--- server.js
+++ server.js
@@ -1,32 +1,33 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 const port = 3000;
 
-app.use('/', express.static(path.join(__dirname, '/../'))); // serve the distribution folder
+// Serve static files (are in the same directory as this file)
+app.use(express.static(path.join(__dirname)));
 
 // Middleware to check we have all the params we need
 const checkParams = (req, res, next) => {
-  if (!Object.prototype.hasOwnProperty.call(req.query, 'speakerPeerId')) {
-    console.log('No peerID given.');
-    throw new Error('No peerID given -- unable to connect to peer.');
+  if (!Object.prototype.hasOwnProperty.call(req.query, "speakerPeerId")) {
+    console.log("No peerID given.");
+    throw new Error("No peerID given -- unable to connect to peer.");
   }
   next();
 };
 
 // Simple Routing
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get('/speaker', (req, res) => {
-  res.sendFile(path.join(__dirname, 'speaker.html'));
+app.get("/speaker", (req, res) => {
+  res.sendFile(path.join(__dirname, "speaker.html"));
 });
 
-app.get('/listener', checkParams, (req, res) => {
-  res.sendFile(path.join(__dirname, 'listener.html'));
+app.get("/listener", checkParams, (req, res) => {
+  res.sendFile(path.join(__dirname, "listener.html"));
 });
 
 app.use((err, req, res, next) => {
@@ -39,7 +40,7 @@ app.use((err, req, res, next) => {
 app.use((req, res) => {
   res.status(404);
   res.send({
-    error: '404 not found',
+    error: "404 not found",
   });
 });
 
